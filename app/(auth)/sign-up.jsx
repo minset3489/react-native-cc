2e4ef7b4afc42e5453068fdee76a3ec9299b7b16
minset3489 <minn.set@gmail.com>
2024-06-27 13:52:54 +0700

    created appwrite config

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -5,6 +5,7 @@ import {images} from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from "expo-router"
+import { createUser } from "../../lib/appwrite"
 
 const SignUp = () => {
 
@@ -17,7 +18,7 @@ const SignUp = () => {
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const submit = () => {
-
+    createUser()
   }
 
   return (
